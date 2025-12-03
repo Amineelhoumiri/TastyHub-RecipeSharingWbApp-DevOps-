f1ca0becc65fac58aa8f3086bf17d75fceb3f0c7
Pitsho <148809797+Amineelhoumiri@users.noreply.github.com>
2025-12-03 21:08:18 +0000

    Fix linting issues in RecipeCard component

--- frontend/app/components/RecipeCard.js
+++ frontend/app/components/RecipeCard.js
@@ -67,6 +67,7 @@ export default function RecipeCard({ recipe }) {
                         onError={() => setImgError(true)}
                     />
                 ) : (
+                    // eslint-disable-next-line @next/next/no-img-element
                     <img
                         src={imgSrc}
                         alt={recipe.title}
